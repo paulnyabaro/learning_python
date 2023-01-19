1d412a72a9df01d5a4721befaca2d1d874cfd0d9
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-19 17:45:57 +0300

    Added approach 2

--- array_sum.py
+++ array_sum.py
@@ -1,7 +1,12 @@
 array = [2, 33, 83, 8, 3, 74, 9, 3, 4, 4]
+array2 = [2, 33, 83, 8, 3, 74, 9, 3, 4, 4]
 
 new_list = [array[0]]
 for i in range(1, len(array)):
     new_list.append(new_list[i - 1] + array[i])
-    
 print(new_list)
+
+# Alternative approach
+for i in range(1, len(array)):
+    array2[i] += array2[i -1]
+print(array2)
