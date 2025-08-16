a70a8ccce2cb3df187787bcc3a797bdad3e3a3e7
paulnyabaro <paulnyabaro1@gmail.com>
2025-08-16 19:44:17 +0300

    Working with classes

--- hello_world_again.py
+++ hello_world_again.py
@@ -3,3 +3,13 @@ print('Hello World again!')
 print('I will be learning pythong again')
 
 print('Testing to see github response')
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def __string__(self):
+        return self.name
+
+    def make_noise(self):
+        print(self.name + 'bark')
